767536e65f4b5e68ec52ee60e3ab30a8cbf1ea75
Tu Nombre <tuemail@example.com>
2026-02-15 23:28:58 -0400

    Updates

--- _next/static/qnI_-rNjAgHcHyJXcNqw7/_buildManifest.js
+++ _next/static/qnI_-rNjAgHcHyJXcNqw7/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/larimar//_next/:path+",
-        "destination": "/larimar/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
